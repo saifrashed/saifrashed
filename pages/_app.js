c1862d2cc2a474ce6da34134f38a2c318b8a5b05
Saif <saifeddinerashed@icloud.com>
2021-09-18 21:44:33 +0200

    fixed fonts

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,10 @@
 import '../styles/globals.css'
 import {ChakraProvider, CSSReset} from "@chakra-ui/react"
 import theme from "../theme"
-import FontFace from "../components/font-face";
-
 
 function MyApp({Component, pageProps}) {
     return (
         <ChakraProvider theme={theme}>
-            <FontFace/>
             <CSSReset/>
             <Component {...pageProps}/>
         </ChakraProvider>
