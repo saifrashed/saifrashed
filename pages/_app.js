b9329f61476595f1e415dbfb0051a65c6bbb21d3
Saif Rashed <saif.rashed@hva.nl>
2022-01-06 21:42:22 +0100

    test GA

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
 import '../styles/globals.css'
 import {ChakraProvider, CSSReset} from "@chakra-ui/react"
 
+import {useEffect} from "react";
+import ReactGA from "react-ga";
+
 function MyApp({Component, pageProps}) {
+
+    useEffect(() => {
+        if(process.env.googleAnalyticsID && process.env.NODE_ENV === "production") { // Checks for GA ID and only turns on GA in production
+            ReactGA.initialize("UA-154460490-1");
+            ReactGA.pageview(window.location.pathname + window.location.search);
+        }
+    });
+
     return (
         <ChakraProvider>
             <CSSReset/>
