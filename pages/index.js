89c1d1f452fd9881db961efe5accb22040ed5db7
Saif Rashed <saif.rashed@hva.nl>
2022-02-23 14:12:29 +0100

    Added apps section on the homepage

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Footer from "../components/Footer"
-import {Box, Flex, Heading, HStack, Stack, Text, useColorModeValue, VStack} from '@chakra-ui/react';
+import {Box, Flex, Heading, HStack, Image, Stack, Tag, Text, useColorModeValue, VStack,} from '@chakra-ui/react';
 import Container from "../components/container";
 import React from "react";
 import Link from "next/link";
@@ -37,7 +37,6 @@ export default function Home() {
                                 </Text>
                             </Box>
 
-
                             <Link href={"/projects"} isExternal>
                                 <HStack
                                     p={4}
@@ -46,6 +45,8 @@ export default function Home() {
                                     cursor="pointer"
                                     borderWidth="1px"
                                     borderColor={useColorModeValue("gray.100", "gray.700")}
+                                    w="100%"
+                                    h="100%"
                                     textAlign="left"
                                     align="start"
                                     spacing={4}
@@ -131,11 +132,77 @@ export default function Home() {
                                     </VStack>
                                 </HStack>
                             </Link>
+
+                            <hr style={{margin: "20px 0px"}}/>
+
+                            <Link href={"https://www.ticketflip.nl"} target={"_blank"} unstyled>
+                                <HStack
+                                    p={4}
+                                    bg={useColorModeValue("white", "gray.800")}
+                                    rounded="xl"
+                                    cursor="pointer"
+                                    borderWidth="1px"
+                                    borderColor={useColorModeValue("gray.100", "gray.700")}
+                                    w="100%"
+                                    h="100%"
+                                    textAlign="left"
+                                    align="start"
+                                    spacing={4}
+                                    transition="all 0.25s"
+                                    transition-timing-function="spring(1 100 10 10)"
+                                    _hover={{transform: "translateY(-4px)", shadow: "sm"}}
+                                >
+                                    <Box
+                                        rounded="lg"
+                                        p={2}
+                                        position="relative"
+                                        overflow="hidden"
+                                        lineHeight={0}
+                                        boxShadow="inset 0 0 1px 1px rgba(0, 0, 0, 0.04)"
+                                    >
+                                        <Box
+                                            position="absolute"
+                                            top={0}
+                                            bottom={0}
+                                            left={0}
+                                            right={0}
+                                            opacity={0.25}
+                                            bg={"#FFECC8"}
+                                        ></Box>
+                                        <Image
+                                            src={"/images/ticketflip.svg"}
+                                            height="50"
+                                            width="50"
+                                            layout="fixed"
+                                            rounded="md"
+                                        ></Image>
+                                    </Box>
+
+                                    <VStack align="start" justify="flex-start" spacing={1}>
+                                        <VStack spacing={0} align="start">
+                                            <HStack>
+                                                <Text fontWeight="bold" fontSize="md" noOfLines={2}>
+                                                    Ticketflip.nl
+                                                </Text>
+                                                <Tag size="sm" colorScheme={"orange"}>
+                                                    Web App
+                                                </Tag>
+                                            </HStack>
+                                            <Text
+                                                fontSize="sm"
+                                                color={useColorModeValue("neutral.1000", "neutralD.1000")}
+                                            >
+                                                All basic components of event management for a low fixed
+                                                transaction fee.
+                                            </Text>
+                                        </VStack>
+                                    </VStack>
+                                </HStack>
+                            </Link>
                         </Stack>
                     </Container>
                 </Box>
             </PageTransition>
-
 
             <Footer/>
         </>
