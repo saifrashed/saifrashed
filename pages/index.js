2a8affceb99aed5810671a203252ddc9efff8bde
Saif Rashed <saifrashed@mbp-van-saif.kpn>
2023-12-06 15:07:22 +0100

    feat: added carstyleai

--- pages/index.js
+++ pages/index.js
@@ -428,7 +428,78 @@ export default function Home() {
 
 
 
+                            <Link href={"https://www.carstyleai.com"} target={"_blank"}>
+                                <Flex
+                                    p={4}
+                                    bg={useColorModeValue("white", "gray.800")}
+                                    rounded="xl"
+                                    cursor="pointer"
+                                    borderWidth="1px"
+                                    borderColor={useColorModeValue("gray.100", "gray.700")}
+                                    w="100%"
+                                    h="100%"
+                                    textAlign="left"
+                                    align="start"
+                                    spacing={4}
+                                    transition="all 0.25s"
+                                    transition-timing-function="spring(1 100 10 10)"
+                                    _hover={{ transform: "translateY(-4px)", shadow: "sm" }}
+                                    display="flex"
+                                    flexWrap="wrap"
+                                    alignItems="center"
 
+                                >
+                                    <Box
+                                        rounded="lg"
+                                        p={2}
+                                        position="relative"
+                                        overflow="hidden"
+                                        lineHeight={0}
+                                        boxShadow="inset 0 0 1px 1px rgba(0, 0, 0, 0.04)"
+                                        flex="none"
+                                        width="50px"
+                                        height="50px"
+                                        marginEnd={"2"}
+                                    >
+                                        <Box
+                                            position="absolute"
+                                            padding={2}
+                                            top={0}
+                                            bottom={0}
+                                            left={0}
+                                            right={0}
+                                            opacity={0.75}
+                                            bg={"#FEF9C3"}
+                                        >
+                                            <Image
+                                                src={"/images/carstyleai.png"}
+
+                                                layout="fixed"
+                                                rounded="md"
+                                            ></Image>
+                                        </Box>
+                                    </Box>
+
+                                    <Box flex="1">
+                                        <VStack align="start" justify="flex-start" spacing={1}>
+                                            <VStack spacing={0} align="start">
+                                                <HStack>
+                                                    <Text fontWeight="bold" fontSize="md" noOfLines={2}>
+                                                        CarstyleAI.com
+                                                    </Text>
+                                                </HStack>
+                                                <Text
+                                                    fontSize="sm"
+                                                    color={useColorModeValue("neutral.1000", "neutralD.1000")}
+                                                >
+                                                    Design your car with artificial intelligence.
+                                                </Text>
+                                            </VStack>
+                                        </VStack>
+                                    </Box>
+
+                                </Flex>
+                            </Link>
 
 
                         </Stack>
